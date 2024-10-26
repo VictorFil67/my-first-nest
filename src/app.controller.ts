8b8dcf4c791afa8a803c156f03dfe2e6857ad1b6
VictorFil67 <victorfilinovych@gmail.com>
2024-10-26 21:12:01 +0300

    completed app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Redirect,
+  Render,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { articles } from './articles';
+import { Article } from './article.model';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get('create')
+  @Render('create-article')
+  getForm(): void {
+    return;
+  }
+
+  @Post('articles')
+  @Redirect('/', 301)
+  create(@Body() body: any): void {
+    const id = articles.length + 1;
+    const article = new Article(body.title, body.content, id);
+    articles.push(article);
+  }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // getHello() {
+  //   console.log('Hello World!');
+  // }
+  @Render('index')
+  index() {
+    return { articles };
+  }
+  @Get(':id')
+  @Render('article')
+  getById(@Param('id', ParseIntPipe) id: number) {
+    return articles.find((article) => article.id === id);
   }
 }
