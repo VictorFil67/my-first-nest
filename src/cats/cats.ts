3bd377a0fb936c60c5ad7ebbbe323ead9311f3ae
VictorFil67 <victorfilinovych@gmail.com>
2024-10-30 20:46:34 +0200

    updated cats array

--- src/cats/cats.ts
+++ src/cats/cats.ts
@@ -2,4 +2,5 @@ import { Cat } from './interfaces/cat.interface';
 export const cats: Cat[] = [
   { name: 'Vasya', age: 6, breed: 'Bombey cat' },
   { name: 'Baziliy', age: 6, breed: 'Half a pers' },
+  { name: 'Whiskers', age: 3, breed: 'Siamese' },
 ];
