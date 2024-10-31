d974e24dbb8b768d2da72af03de873fd5bfaed5d
VictorFil67 <victorfilinovych@gmail.com>
2024-10-31 23:16:52 +0200

    created entity flowers

--- src/flowers/flowers.service.ts
+++ src/flowers/flowers.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class FlowersService {}
+export class FlowersService {
+  findAll() {
+    return [
+      { name: 'rose', color: 'red', price: 5 },
+      { name: 'tulip', color: 'white', price: 6 },
+      { name: 'daisy', color: 'yellow', price: 7 },
+    ];
+  }
+}
