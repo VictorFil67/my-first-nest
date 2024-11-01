b5f1f1ba7f815a35b4e6179399863c77a4542350
VictorFil67 <victorfilinovych@gmail.com>
2024-11-01 19:32:28 +0200

    added pipe

--- src/flowers/flowers.controller.ts
+++ src/flowers/flowers.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { FlowersService } from './flowers.service';
+import { ParseIntPipe } from 'src/conception/parseIntPipe';
 
 @Controller('flowers')
 export class FlowersController {
   constructor(private readonly flowersService: FlowersService) {}
   @Get()
-  findAll() {
+  findAll(@Query('pageNumber', ParseIntPipe) pageNumber: number) {
+    console.log(pageNumber);
     console.log('after Request');
     return this.flowersService.findAll();
   }
